4fabae661ac1d237e0825f34c6df3da8d395ab92
gabriel-almeida <gabriel-almeida@users.noreply.github.com>
2021-11-13 01:38:01 +0000

    deploy: 64d773ec945c56df63a8e26385643f8e443d343a

---
+++ _nuxt/static/1636767480/summarizer/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636767480",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsummarizer",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c73db6c3"}}}}(null));
