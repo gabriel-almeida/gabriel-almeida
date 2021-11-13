cf5fad2abfa0a674fb99943d1043a148485b1bde
gabriel-almeida <gabriel-almeida@users.noreply.github.com>
2021-11-13 00:35:23 +0000

    deploy: 166a47e0950d2c9b37124ea15a91d18eb4c54819

---
+++ _nuxt/static/1636763722/blog/placeholder-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636763722",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fplaceholder-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cb0396eb"}}}}(null));
---
+++ _nuxt/static/1636763722/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fsummarizer","\u002Fblog\u002Fplaceholder-2","\u002Fblog\u002Fplaceholder-1"]})
