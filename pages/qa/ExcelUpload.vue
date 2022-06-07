76dc3885ecb266a4ac587265ef2dcda7184336f7
Gabriel Garcia de Almeida <gabrielalmeida@tic.ufrj.br>
2022-06-07 09:30:28 -0300

    feat: parsing answers

--- pages/qa/ExcelUpload.vue
+++ pages/qa/ExcelUpload.vue
@@ -4,6 +4,7 @@
   </div>
 </template>
 <script>
+const CORRECT_REGEXP = /(sim|correto|certo|1)/
 const ExcelJS = require('exceljs')
 
 export default {
@@ -34,14 +35,23 @@ export default {
       const result = []
 
       sheet.eachRow((row) => {
-        const cell = row.getCell(1)
-        const question = { content: cell.text, expected: true }
-        result.push(question)
+        const questionCell = row.getCell(1)
+        const answerCell = row.getCell(2)
+
+        const content = questionCell.text
+        const expected = this.parseAnswer(answerCell.text)
+
+        result.push({ content, expected })
       })
 
       this.$emit('input', result)
       //   this.$forceUpdate()
     },
+
+    /** @param {string} text */
+    parseAnswer(text) {
+        return !!text && CORRECT_REGEXP.test(text)
+    }
   },
 }
 </script>
